ed305ef9c8e14c1b5d21cd03552e2aaf313678e0
Cecilia <ceciliachiouu@gmail.com>
2020-07-29 12:14:43 -0700

    Gave each view controller their own separate file

---
+++ SimplyYummy_Project/ViewController_Potato Salad.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController_Potato Salad.swift
+//  SimplyYummy_Project
+//
+//  Created by Apple on 7/29/20.
+//  Copyright © 2020 Cecilia Chiou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController_Potato_Salad: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
