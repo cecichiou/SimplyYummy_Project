2bad921c35666b12455fef1d9fce4fc3f7e4dc77
Cecilia <ceciliachiouu@gmail.com>
2020-07-30 13:36:24 -0700

    Day 3 - Worked on the Search Bar area, and added code for the images to appear (not working yet)

--- SimplyYummy_Project/ViewController_SearchByIngredients.swift
+++ SimplyYummy_Project/ViewController_SearchByIngredients.swift
@@ -14,15 +14,41 @@ class ViewController_SearchByIngredients: UIViewController {
         super.viewDidLoad()
         TunaMacSalad_Search.isHidden = true
         PotatoSalad_Search.isHidden = true
+        Mac_Search.isHidden = true
 
         
+    }
+    @IBAction func SearchBtn(_ sender: Any) {
+        if SearchBar.text == "Macaroni" {
+            Mac_Search.isHidden = !Mac_Search.isHidden
+            TunaMacSalad_Search.isHidden = !TunaMacSalad_Search.isHidden
+        } else if SearchBar.text == "Mayonaise" {
+                TunaMacSalad_Search.isHidden = !TunaMacSalad_Search.isHidden
+                PotatoSalad_Search.isHidden = !PotatoSalad_Search.isHidden
+        } else if SearchBar.text == "Salt" {
+            Mac_Search.isHidden =  !Mac_Search.isHidden
+            TunaMacSalad_Search.isHidden = !TunaMacSalad_Search.isHidden
+            PotatoSalad_Search.isHidden = !PotatoSalad_Search.isHidden
+        } else if SearchBar.text == "Pepper" {
+            Mac_Search.isHidden = !Mac_Search.isHidden
+            TunaMacSalad_Search.isHidden = !TunaMacSalad_Search.isHidden
+            PotatoSalad_Search.isHidden = !PotatoSalad_Search.isHidden
+        }
+            
     }
     
     @IBOutlet weak var TunaMacSalad_Search: UIImageView!
     
     @IBOutlet weak var PotatoSalad_Search: UIImageView!
     
-    }
+    @IBOutlet weak var
+    Mac_Search: UIImageView!
+    
+    @IBOutlet weak var SearchBar: UITextField!
+    
+    
+    
+}
     
 
 
