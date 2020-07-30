7e8e14a6219601c74947d118f914ebf878794785
Cecilia <ceciliachiouu@gmail.com>
2020-07-30 11:55:14 -0700

    Day 3 - Finish Basic Layout of everything, except for code, might end up just not being able to code the search for ingredient area or the add your own recipe area

--- SimplyYummy_Project/ViewController_AddYourOwnRecipe.swift
+++ SimplyYummy_Project/ViewController_AddYourOwnRecipe.swift
@@ -8,23 +8,43 @@
 
 import UIKit
 
-class ViewController_AddYourOwnRecipe: UIViewController {
+class ViewController_AddYourOwnRecipe: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
 
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func homepageLibraryButtonTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+            
+            present(imagePicker, animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var displayHomepageImage: UIImageView!
+        
+    @IBAction func recipePageLibraryButtonTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+            
+            present(imagePicker, animated: true, completion: nil)
     }
-    */
+    
+    @IBOutlet weak var displayRecipeImage: UIImageView!
+    
 
-}
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            displayHomepageImage.image = selectedImage
+        }
+        if let selectedImage2 = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                   displayRecipeImage.image = selectedImage2
+               }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    
+    }
+
