22865028a2b79829e32a3f45e67e532b9ac2ae19
Cecilia <ceciliachiouu@gmail.com>
2020-07-30 11:13:38 -0700

    Day3 - Added in the Mac&Cheese Recipe, Added View Controllers for Searching Up By Ingredient (May or may not work), and Two View Controllers to allow user to add in their own recipe

---
+++ SimplyYummy_Project/ViewController_SearchByIngredients.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController_SearchByIngredients.swift
+//  SimplyYummy_Project
+//
+//  Created by Apple on 7/30/20.
+//  Copyright © 2020 Cecilia Chiou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController_SearchByIngredients: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TunaMacSalad_Search.isHidden = true
+        PotatoSalad_Search.isHidden = true
+
+        
+    }
+    
+    @IBOutlet weak var TunaMacSalad_Search: UIImageView!
+    
+    @IBOutlet weak var PotatoSalad_Search: UIImageView!
+    
+    }
+    
+
+
